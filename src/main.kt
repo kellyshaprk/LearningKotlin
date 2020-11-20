729090fd13c9702299fdf19f5a1a5256cc7005e8
Kelly <kelly.shpark@gmail.com>
2020-11-19 21:23:12 -0500

    for loop

--- src/main.kt
+++ src/main.kt
@@ -1,38 +1,31 @@
 fun main(args: Array<String>) {
-    // kotlin은 null 오류를 처리하기 쉬움
-    // 기본적으로 String은 null을 허용하지 않지만 데이터 타입 뒤에 물음표를 넣으면 null을 기본값으로 넣을 수 있음
-    var nullValue: String? = null
-    println(nullValue)
+    val colors = arrayOf("Red", "Green", "Blue")
+    val values = intArrayOf(1,3,5,7,9)
 
-    // Int도 마찬가지. 그런데 여기서 nullValue의 length라니 말도 안되는 코드인데
-    // nullValue에도 물음표를 붙여주면 Int 형 변수값도 null이 됨
-    val l:Int? = nullValue?.length
-    println("The length is $l")
-
-    if (l == null) {
-        println("Length is null")
+    printHeader("For each loop")
+    for (color in colors) {
+        println(color)
     }
 
-    val message = if(l != null) {
-        "Length is $l"
-    } else {
-        "l is null"
+    for (value in values) {
+        println(value)
     }
-    println(message)
+    printHeader("For loop with indices")
 
-    // l의 값을 l2에 넣어줌
-    // l2의 값이 null이면 -1, null이 아니면 l2 본인의 값을 출력
-    val l2:Int = l ?: -1
-    println("The value of l2 is $l2")
-
-    try {
-        // null assertion
-        // 변수값이 null이 아닌것을 확신할때 느낌표 두개 붙여줌
-        // 그런데 null이라면? 오류 방지를 위해 try-catch 를 사용함
-        val l3 = l!!
-        println("The value of l3 is $l3")
-    } catch (e: java.lang.NullPointerException) {
-        println("l3 is null")
+    // index 의 복수형 indices
+    // step 2: instead of seeing every item in the array, I'll see every other item
+    for (i in values.indices step 2){
+        println(values[i])
     }
 
+    // 또는 0..colors.size-1: colors.size 는 3인데, 0부터 3까지 하면 indexOutOfBounds 오류 남, 이에 -1을 붙여줌
+    for (i in 0 until colors.size) {
+        println(colors[i])
+    }
+}
+
+fun printHeader(label: String) {
+    println("**************")
+    println(label)
+    println("**************")
 }
