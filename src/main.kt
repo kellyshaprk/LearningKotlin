def641882fe110f4257ccd406943e184d6ab8288
Kelly <kelly.shpark@gmail.com>
2020-11-20 23:31:53 -0500

    iterate

--- src/main.kt
+++ src/main.kt
@@ -1,27 +1,19 @@
 fun main(args: Array<String>) {
-    val colors = arrayOf("Red", "Green", "Blue")
-    val values = intArrayOf(1,3,5,7,9)
+    val states = arrayOf("CA", "KR", "NA")
 
-    printHeader("For each loop")
-    for (color in colors) {
-        println(color)
+    var counter = 0
+/*    while(counter < states.size) {
+        println("counter = $counter")
+        println("state = ${states.get(counter)}")
+        counter ++
     }
-
-    for (value in values) {
-        println(value)
-    }
-    printHeader("For loop with indices")
-
-    // index 의 복수형 indices
-    // step 2: instead of seeing every item in the array, I'll see every other item
-    for (i in values.indices step 2){
-        println(values[i])
-    }
-
-    // 또는 0..colors.size-1: colors.size 는 3인데, 0부터 3까지 하면 indexOutOfBounds 오류 남, 이에 -1을 붙여줌
-    for (i in 0 until colors.size) {
-        println(colors[i])
-    }
+    */
+    counter = 0
+    printHeader(" Do / While loop")
+    do {
+        println("states = ${states.get(counter)}")
+        counter ++
+    } while(counter < states.size)
 }
 
 fun printHeader(label: String) {
